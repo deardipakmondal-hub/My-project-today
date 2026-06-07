7b479c57b05ca82db87557162b88b1d56f30fa25
deardipakmondal-hub <deardipakmondal@gmail.com>
2026-06-07 15:23:07 +0530

    Correct Firebase URL casing

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ API_TOKEN = '8675792162:AAECQMno0-Rdk4QM7z0tAqe410fcYtj0Sqw'
 ADMIN_GROUP_ID = -1003758213595
 KOLKATA = pytz.timezone('Asia/Kolkata')
 # 🔥 Aapka Singapore region wala Firebase URL yahan bilkul sahi format mein set hai:
-FIREBASE_DB_URL = 'https://fullmap-903ce-default-rtdb.asia-southeast1.firebasedatabase.app/'
+FIREBASE_DB_URL = 'Https://fullmap-903ce-default-rtdb.asia-southeast1.firebasedatabase.app/'
 
 # --- FIREBASE INITIALIZE ---
 cred = credentials.Certificate("credentials.json")
